986534b23ab1753411769ebc22dd74621075387a
Mark Coetzer <u14044537@tuks.co.za>
2018-04-17 20:35:48 +0200

    Basic drop down team list working

--- my-owl-app/src/app/team-list/team-list.component.ts
+++ my-owl-app/src/app/team-list/team-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TEAMS } from '../shared/mock_data/teams.mock';
+
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
   styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
+
+  teams = TEAMS;
+
 
   constructor() { }
 

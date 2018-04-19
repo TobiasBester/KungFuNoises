4c4fdfe7dac171cf97ba59a8430672e1f7913dcd
Tobias Bester <u14041368@tuks.co.za>
2018-04-19 20:12:43 +0200

    Merge branch fix nothing big

--- my-owl-app/src/app/app.module.ts
+++ my-owl-app/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
 import { TeamListComponent } from './team-list/team-list.component';
 import { ThemeSelectComponent } from './theme-select/theme-select.component';
+import { TeamModalComponent } from './team-modal/team-modal.component';
+import { TeamModalService } from './team-modal.service';
+import { PlayerModalService } from './player-modal.service';
+import { PlayerModalComponent } from './player-modal/player-modal.component';
 
 @NgModule({
   declarations: [

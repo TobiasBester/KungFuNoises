2abcb7e27f1e461ec8a622d7685cbcdaa1b7b879
Mark Coetzer <u14044537@tuks.co.za>
2018-04-19 23:54:54 +0200

    Fixed signup routing

--- my-owl-app/src/app/app-routing.module.ts
+++ my-owl-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full'},
   { path: 'landing', component: LandingComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignUpComponent }
 ];
 
 @NgModule({

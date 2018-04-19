c98e8c951caac0a329dc3ac3723b58b50369454f
Mark Coetzer <u14044537@tuks.co.za>
2018-04-19 19:03:28 +0200

    Modal loads selectable buttons with team names

--- my-owl-app/src/app/team-modal/team-modal.component.ts
+++ my-owl-app/src/app/team-modal/team-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { TEAMS_LIST } from '../shared/mock_data/teams.mock';
 
 @Component({
   selector: 'app-team-modal',
@@ -7,6 +8,8 @@ import { MatDialogRef } from '@angular/material';
   styleUrls: ['./team-modal.component.css']
 })
 export class TeamModalComponent implements OnInit {
+
+  teams = TEAMS_LIST;
 
   constructor(public dialogRef: MatDialogRef<TeamModalComponent>) { }
 
